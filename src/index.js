f401bb973dbc33a2af81dfe658db6851cf762f32
cineminnam <cine.minnam@gmail.com>
2020-04-12 21:20:48 +0900

    #1.3 Subscriptions
    #1.3 Subscriptions

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+import {createStore} from "redux";
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count=0, action) => {
+    if (action.type === "PLUS"){
+        return count+1;
+    }else if (action.type === "MINUS"){
+        return count-1;
+    }else{
+        return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handlePlus = () => {
+    countStore.dispatch({type: "PLUS"});
+}
+
+const handleMinus = () => {
+    countStore.dispatch({type: "MINUS"});
+}
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
