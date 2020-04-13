ec30cf4718615f93cb309e27ab765a092140cb3d
cineminnam <cine.minnam@gmail.com>
2020-04-13 13:14:12 +0900

    #4.1 createAction
    #4.1 createAction

--- src/store.js
+++ src/store.js
@@ -1,28 +1,15 @@
 import {createStore} from "redux";
+import {createAction} from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    }
-} 
-
-const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id: parseInt(id)
-    }
-}
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state=[], action) => {
     switch(action.type){
-        case ADD:
-            return [...state, {text: action.text, id: Date.now()}];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        case addToDo.type:
+            return [...state, {text: action.payload, id: Date.now()}];
+        case deleteToDo.type:
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             return state;
     }
@@ -34,6 +21,5 @@ export const actionCreators = {
     addToDo,
     deleteToDo
 }
-// store.subscribe();
 
 export default store;
